aeeb339dcfa8c35afa5359456c9bfde5759018fa
Josip Igrec <josip@igr.ec>
2023-04-07 14:46:33 +0200

    Directly use `ReactQueryDevtools` because its auto stripped in prod

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import "~/styles/globals.css";
 
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { type AppContext, type AppType } from "next/app";
 import NextApp from "next/app";
 import Head from "next/head";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import { lazy, Suspense } from "react";
+import { Suspense } from "react";
 import { CookiesProvider } from "react-cookie";
 import UniversalCookie from "universal-cookie";
 
@@ -13,15 +14,6 @@ import { MainLayout } from "~/layouts/main";
 import { type NextPageWithLayout } from "~/types/layout";
 import { api } from "~/utils/queryApi";
 
-const ReactQueryDevtools = lazy(() =>
-  "development" === process.env.NODE_ENV
-    ? import("@tanstack/react-query-devtools").then((mod) => ({
-        default: mod.ReactQueryDevtools,
-      }))
-    : Promise.resolve({
-        default: () => null,
-      }),
-);
 const getInitialProps = async (app: AppContext) => {
   return {
     ...(await NextApp.getInitialProps(app)),
