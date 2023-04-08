ee8e5c9d5f62ee08fdb694690965f77d1c227c69
Josip Igrec <josip@igr.ec>
2023-04-08 14:22:25 +0200

    Add SEO to pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import NextApp from "next/app";
 import Head from "next/head";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { DefaultSeo } from "next-seo";
 import { CookiesProvider } from "react-cookie";
 import UniversalCookie from "universal-cookie";
 
@@ -38,7 +39,34 @@ const MyApp: AppType<{
     <>
       <Head>
         <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=Edge" />
+        <meta
+          name="keywords"
+          content="KSET, klub, studenti, fakultet, zagreb, elektrotehnika, računarstvo, hacker, volunteering, computing, electrical engineering, club, nightlife, university"
+        />
+        <meta property="og:latitude" content="45.80154" />
+        <meta property="og:longitude" content="15.97166" />
+        <meta property="og:street-address" content="Unska 3" />
+        <meta property="og:locality" content="Zagreb" />
+        <meta property="og:region" content="Zagreb" />
+        <meta property="og:postal-code" content="10000" />
+        <meta property="og:country-name" content="Croatia" />
       </Head>
+      <DefaultSeo
+        titleTemplate="KSET.org - %s"
+        title="Klub studenata elektrotehnike"
+        description="Klub studenata elektrotehnike, poznatiji kao KSET je zagrebački klub i okupljalište studenata Fakulteta elektrotehnike i računarstva (FER)."
+        openGraph={{
+          type: "website",
+          locale: "hr_HR",
+          url: "https://www.kset.org/",
+          siteName: "KSET.org",
+        }}
+        twitter={{
+          cardType: "summary_large_image",
+        }}
+      />
       <CookiesProvider cookies={cookies}>
         <SessionProvider session={session}>
           {getLayout ? (
