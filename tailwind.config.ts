22f41f6a6125cec20d62099468d18be109bd57b8
Josip Igrec <josip@igr.ec>
2023-08-07 00:22:22 +0200

    Fix tailwind setting line-height per font-size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,28 @@
+import { fromPairs, toPairs } from "rambdax";
 import { type Config } from "tailwindcss";
+import defaultConfig from "tailwindcss/defaultConfig";
+
+type FontSizeRaw = NonNullable<NonNullable<Config["theme"]>["fontSize"]>;
+type FontSize = Exclude<FontSizeRaw, (...args: never[]) => unknown>;
+
+const fontSize = fromPairs(
+  toPairs((defaultConfig.theme?.fontSize ?? {}) as unknown as FontSize).map(
+    ([key, value]) => {
+      if (typeof value === "string") {
+        return [key, value];
+      }
+
+      const [size, _config] = value;
+
+      return [key, size];
+    },
+  ),
+) satisfies FontSize;
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontSize,
     extend: {
       colors: {
         black: "#1a1a1a",
